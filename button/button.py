2009b90cd93cdfd4052e41e38023d9f15b3360a7
robot <Zupken@spoko.pl>
2018-06-04 17:33:42 +0200

    Replaced number of pin on button.py.

--- button/button.py
+++ button/button.py
@@ -3,11 +3,11 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)#Button to GPIO23
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)#Button to GPIO18
 
 try:
     while True:
-         button_state = GPIO.input(23)
+         button_state = GPIO.input(18)
          if button_state == False:
              print('Button Pressed...')
              time.sleep(0.2)
